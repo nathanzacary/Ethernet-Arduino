a10bdfd0096da43fc050a829abe7d385eff9dfe3
Simon Brüchner <powtac@gmx.de>
2015-12-30 15:18:23 +0100

    Clean up, still working version

--- Client/Client.ino
+++ Client/Client.ino
@@ -17,9 +17,11 @@ void setup() {
   }
   Serial.println("Client starts...");
 
+
+
   Ethernet.begin(mac0, ip0/*, gateway, subnet*/);
   delay(1000);
-  
+
   Serial.println("Try to connect to "+ip_to_string(ip1)+":"+httpport+" MAC "+mac_to_string(mac1));
 
   if (client.connect(ip1, httpport)) {
@@ -29,50 +31,34 @@ void setup() {
     Serial.println(client.status(), DEC);
     Serial.println("Please reset \"Client\" Arduino Board until connection is good.");
   }
-  // }
 }
 
 void loop()
 {
-  // if there are incoming bytes available
-  // from the server, read them and print them:
-  // the server code above doesn't send anythingâ€¦
-  // but if it did, this is where you would echo it
   int ii;
 
-  delay(250);
-  
   // char c = client.read();
   // Serial.print("***Server says:***\n");
   // Serial.print(c);
 
-  // assume your variable myVar will have a valid string in it...
+
   strcpy(myVar, "123.456\n");
-  // tell the serial port what you are sending:
+  
+
   Serial.print("sending variable: ");
   Serial.print(myVar);
   for (ii = 0; ii < strlen(myVar); ii++) {
-    if (client.connected()) {
+    if (client.connected()) { // wieso??? wenn disconnected werden Zeichen übersprungen!
       client.print(myVar[ii]);
     }
   }
-  //client.print("\n");
-  //client.println();
-  //client.stop();
 
-  
-  // if the server's disconnected, stop the client:
- 
   if (!client.connected()) {
     Serial.println();
-    Serial.println("disconnecting.");
+    Serial.println("disconnected/disconnecting...");
     client.stop();
     client.connect(ip1, httpport);
     delay(500);
-    // do nothing:
-    // while (true);
   }
-  
-  // add appropriate delay here before sending next data element
 }
 
