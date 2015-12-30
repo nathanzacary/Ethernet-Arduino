6d638a4158468a241bd0903c0174849b9477030d
Simon Brüchner <powtac@gmx.de>
2015-12-30 13:06:45 +0100

    Clean up

--- Client/Client.ino
+++ Client/Client.ino
@@ -1,47 +1,21 @@
-// http://stackoverflow.com/questions/19920796/how-to-have-2-arduinos-communicate-a-variable-over-ethernet
+// Client
 
+#include "config.h"
 #include <SPI.h>
 #include <Ethernet.h>
-// inspired by/copied from http://arduino.cc/en/Tutorial/TelnetClient
 
-// Enter a MAC address and IP address for your controller below:
-// The IP address will be dependent on your local network:
-byte mac[] = { 0xDE, 0xAD, 0xDE, 0xEF, 0xFE, 0xED };
-// IPAddress ip(192,168,1,176);  // this is the data source card IP address
-IPAddress ip(192, 168, 188, 116); //Client = COM16 daher ip-Adresse: 192.168.188.116
 
-// the IP address of the server you're connecting to:
-IPAddress server(192, 168, 188, 115);
-
-// Initialize the Ethernet client library
-// with the IP address and port of the server
-// that you want to connect to (port 23 is default for telnet;
 EthernetClient client;
-int port = 80;   // telnet default port
+
 
 char myVar[100]; // contains string with variable to transmit
 
 void setup() {
-  // start the Ethernet connection:
-  Ethernet.begin(mac, ip);
-  // Open serial communications and wait for port to open:
   Serial.begin(115200);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
-
-  // give the Ethernet shield a second to initialize:
-  delay(1000);
-  Serial.println("connecting...");
-
-  // if you get a connection, report back via serial:
-  if (client.connect(server, port)) {
-    Serial.println("connected");
-  }
-  else {
-    // if you didn't get a connection to the server:
-    Serial.println("connection failed");
-  }
+  Serial.println("Client starts...");
 }
 
 void loop()
@@ -51,11 +25,23 @@ void loop()
   // the server code above doesn't send anythingâ€¦
   // but if it did, this is where you would echo it
   int ii;
-  if (client.available()) {
-    char c = client.read();
+  while (!client.available()) {
+    Ethernet.begin(mac0, ip0, gateway, subnet);
+    
+    delay(1000);
+    
+    Serial.println("Try to connect to "+ip_to_string(ip1)+":"+httpport+" MAC "+mac_to_string(mac1));
+  
+    if (client.connect(ip1, httpport)) {
+      Serial.println("connected");
+    } else {
+      Serial.println("connection failed");
+    }
+  }
+
+     char c = client.read();
     Serial.print("***Server says:***\n");
     Serial.print(c);
-  }
 
   // assume your variable myVar will have a valid string in it...
   strcpy(myVar, "123.456\n");
