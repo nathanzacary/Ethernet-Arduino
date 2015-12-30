d5b7757daa10e7e5d8cbea235c5b324737df4c05
Simon Brüchner <powtac@gmx.de>
2015-12-30 22:35:30 +0100

    Splitting values on Server into useful data

--- Server/Server.ino
+++ Server/Server.ino
@@ -32,8 +32,28 @@ void loop()
   
   if (client) {
     command = client.readStringUntil('\n');
-    Serial.print(command);
-    Serial.println();
+    command.toUpperCase();
+    // Serial.print(command);
+    // Serial.println();
+
+    // Motor Signal, zB. "A3v501" -> Motor 4 Wert 501
+    if (command.startsWith("A")) {
+      int vPos = command.indexOf("V");
+      String m = command.substring(1, vPos);
+      String v = command.substring(vPos+1);
+      v = v.toInt();
+      Serial.println("Motor: "+m+" setze "+v.toInt());
+    } else {
+      Serial.println("\""+command+"\" ist kein Motor Signal");
+    }
+
+
+
+
+
+
+
+    
   }
 }
 
