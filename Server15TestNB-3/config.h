3443b0b40c81b8ab83361f7a5e01fb3c80aac6e6
Simon Brüchner <powtac@gmx.de>
2015-12-30 12:07:43 +0100

    Adding global config for both parts Server and Client

---
+++ Server15TestNB-3/config.h
@@ -0,0 +1,24 @@
+
+// Client / Sender / ETH0 / UNO0
+byte mac0[]     = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+byte ip0[]      = { 192, 168, 188, 115 };
+int a0          = A0; // Analog Port
+int a1          = A1;
+int a2          = A2;
+int a3          = A3;
+
+// Server / Receiver / ETH1 / UNO1
+byte mac1[]     = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEC };
+byte ip1[]      = { 192, 168, 188, 116 };
+int m0          = 3; // PWM Port
+int m1          = 5;
+int m2          = 6;
+int m3          = 9;
+
+
+// Wifi Router
+byte gateway[]  = { 10, 0, 0, 1 };
+byte subnet[]   = { 255, 255, 0, 0 };
+
+
+
