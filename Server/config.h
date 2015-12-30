da7d4599776b94ca2260564f35709b26eb89685e
Simon Brüchner <powtac@gmx.de>
2015-12-30 22:19:07 +0100

    Ethernet blocks A0/A2!

--- Server/config.h
+++ Server/config.h
@@ -2,10 +2,10 @@
 // Client / Sender / ETH0 / UNO0
 byte mac0[]     = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 byte ip0[]      = { 192, 168, 188, 115 };
-int a0          = A0; // Analog Port
-int a1          = A1;
-int a2          = A2;
-int a3          = A3;
+int pina0          = A2; // Analog Port
+int pina1          = A3;
+int pina2          = A4;
+int pina3          = A5;
 
 // Server / Receiver / ETH1 / UNO1
 byte mac1[]     = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEC };
@@ -34,5 +34,16 @@ String mac_to_string(byte mac[6]) {
   return (String(mac[0], HEX) + String(":") + String(mac[1], HEX) + String(":") + String(mac[2], HEX) + String(":") + String(mac[3], HEX) + String(":") + String(mac[4], HEX) + String(":") + String(mac[5], HEX));
 }
 
+// Additional information
+/*
+ * Note that because the W5100 and SD card share the SPI bus, 
+ * only one can be active at a time. If you are using both peripherals 
+ * in your program, this should be taken care of by the corresponding 
+ * libraries. If you're not using one of the peripherals in your 
+ * program, however, you'll need to explicitly deselect it. To do this 
+ * with the SD card, set pin 4 as an output and write a high to it. 
+ * For the W5100, set digital pin 10 as a high output. 
+ * /
+ */
 
 
